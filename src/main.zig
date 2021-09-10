a15de254ed50f8e3f89da490cadb4faefb42dbcb
damilolarandolph <damilolarandolph@gmail.com>
2021-09-10 03:14:22 +0000

    VESA working (white line).

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
-const video = @import("drivers/video.zig");
+const vbe = @import("drivers/video/vbe.zig");
+extern const vbe_info: u32;
 
-pub export fn main() void {
-    video.printChar('X');
+pub export fn main(val: u32) callconv(.C) void {
+    var vbePtr = @intToPtr(*vbe.VesaBlock, vbe.VBE_ADDRESS);
+    var frameBufferPtr = @intToPtr([*]u24, vbePtr.physicalBasePtr);
+    var frameBufferPtr8 = @intToPtr([*]u8, vbePtr.physicalBasePtr);
+
+    for (frameBufferPtr8[0..100]) |*b| {
+        b.* = 0xff;
+    }
+
     while (true) {}
 }
